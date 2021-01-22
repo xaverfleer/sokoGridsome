f0353f196df389d1b721c0297c2a0f8071b9f826
Xaver Fleer <xaver.fleer@gmail.com>
2021-01-22 22:17:56 +0100

    Add first section

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,26 +1,104 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur
-      excepturi labore tempore expedita, et iste tenetur suscipit explicabo!
-      Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
-        >Gridsome Docs</a
-      >
-      <a
-        href="https://github.com/gridsome/gridsome"
-        target="_blank"
-        rel="noopener"
-        >GitHub</a
-      >
-    </p>
+      <section class="section hero">
+        <p class="hero__supertitle">
+          Onlinekurs für Eltern, Bezugs- und&nbsp;Betreuungspersonen
+        </p>
+        <h1 class="hero__title">
+          <em>so*</em> kommunizieren mit meinem&nbsp;Baby
+        </h1>
+        <p class="hero__subjektorientiert">
+          <em>*subjektorientiert: einfühlsam, wertschätzend, stärkend</em>
+        </p>
+        <br />
+        <p>
+          <em>Einfühlsam, wertschätzend, stärkend:</em> wenn wir
+          <em>so*</em> mit unserem Baby kommunizieren, treten wir in eine
+          Beziehung zu unserem Baby, die nährend für seine gesunde Entwicklung
+          ist. In der Wissenschaft ist man sich heute einig, dass die ersten
+          drei Lebensjahre die künftige Entwicklung eines Kindes wahnsinnig
+          stark und zwar nicht irreversibel aber doch sehr nachhaltig
+          beeinflussen. In dieser ersten Zeit können Eltern und andere
+          Bezugspersonen eine stabiles Fundament für die gesunde Entwicklung
+          ihrer Kinder bis weit ins Erwachsenenalter schaffen. Deswegen geht es
+          in diesem Onlinekurs um genau diese erste wichtige Zeit.
+        </p>
+        <p>
+          Der Onlinekurs «<em>so*</em> kommunizieren mit meinem Baby» vermittelt
+          in anschaulichen Videos grundlegendes Wissen darüber, was Babys für
+          eine gesunde Entwicklung brauchen. Dieses Wissen wird mit ganz
+          konkreten Strategien für die Kommunikation und Interaktion mit Babys
+          im Alltag verbunden.
+        </p>
+        <div class="section__subsection--highlight">
+          <h2>Early-Birds gesucht</h2>
+          <p>
+            Dieser Onlinekurs befindet sich noch im Aufbau.
+            <em>DEIN</em> Feedback ist mir wichtig! Deswegen suche ich
+            neugierige Eltern oder Bezugspersonen, die fortlaufend meine neuen
+            Kursinhalte testen. Werde zum Early-Bird und profitiere jetzt von
+            einem einmaligen Rabatt von 80%, wenn du...
+          </p>
+          <ul class="ul--3-dots">
+            <li>
+              Elternteil oder Bezugsperson eines Babys zwischen 0 und 2 Jahren
+              bist,
+            </li>
+            <li>
+              dich für die einfühlsame, wertschätzende, stärkende Kommunikation
+              mit Babys interessierst,
+            </li>
+            <li>
+              uns mit deinem Feedback zu unseren Kursinhalten weiterhelfen
+              willst.
+            </li>
+          </ul>
+          <p>
+            Die ersten Personen, die sich für unser Early Bird Paket anmelden,
+            ...
+          </p>
+          <ul class="ul--3-dots">
+            <li>profitieren von einem Rabatt von 80%,</li>
+            <li>erhalten sofort Zugriff auf alle verfügbaren Inhalte,</li>
+            <li>
+              erhalten fortlaufend und dauerhaft Zugriff auf alle neuen und
+              überarbeiteten Kursinhalte.
+            </li>
+          </ul>
+          <div class="buttons">
+            <a
+              class="button button--primary"
+              href="./kurs.html"
+              target="_blank"
+              title="Gratis starten"
+            >
+              Gratis starten
+            </a>
+            <a
+              class="button button--2ndary cta05"
+              href="#prizing"
+              title="Jetzt buchen"
+              >Jetzt buchen</a
+            >
+          </div>
+          <g-image
+            class="early-bird__icon"
+            height="64"
+            width="64"
+            src="~/assets/early-birds.svg"
+          />
+        </div>
+        <div class="vimeo-video__container">
+          <iframe
+            class="vimeo-video__iframe"
+            src="https://player.vimeo.com/video/405472720"
+            frameborder="0"
+            allow="autoplay; fullscreen"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </section>
+    </main>
   </Layout>
 </template>
 
@@ -33,7 +111,134 @@ export default {
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+.button {
+  /* border: none; */
+  /* font-size: 1.4rem; */
+  letter-spacing: 0.1em;
+  text-transform: uppercase;
+  text-decoration: none;
+  border-radius: 5px;
+  /* max-width: 12em; */
+  padding: 0.75em;
+  /* min-height: 46px; */
+  /* display: flex; */
+  /* align-items: center; */
+  /* justify-content: center; */
+}
+
+.button--2ndary {
+  color: #4ba747;
+  font-weight: 800;
+  margin: 0 0.5em;
+}
+
+.button--2ndary:hover {
+  color: #408f3d;
+}
+
+/*.button--gdpr-ignore {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 20px;
+  height: 20px;
+  background-image: url(imgs/close-24px--brown.svg);
+  background-repeat: no-repeat;
+  background-color: transparent;
+  background-position: center;
+  border: none;
+  margin: 7px;
+} */
+
+.button--primary,
+.button--primary a:any-link {
+  background-color: #4ba747;
+  font-weight: 500;
+  color: #ffffff;
+}
+
+/*.button--primary a:any-link {
+  background-color: transparent;
+}
+
+.button--primary:hover {
+  background-color: #408f3d;
+}
+
+.button--sameline {
+  max-height: 46px;
+}
+
+.button.button--left {
+  padding-left: 0;
+} */
+
+.buttons {
+  display: flex;
+  justify-content: center;
+  margin: 1em;
+  /* text-align: flex-end; */
+}
+
+.early-bird__icon {
+  position: absolute;
+  top: -48px;
+  left: 75%;
+}
+
+.hero__subjektorientiert {
+  margin: 0;
+}
+
+.hero__supertitle {
+  font-size: 2rem;
+  color: #966b40;
+  margin-bottom: 0.1rem;
+}
+
+.hero__title {
+  margin: 0.1rem 0;
+}
+
+.main {
+  margin: 1em 0 3em;
+}
+
+.section {
+  padding: 2em 1em 1em;
+  /* border-radius: 5px; */
+}
+
+.section:nth-of-type(2n) {
+  /* background-color: #f6ede2; */
+}
+
+.section:nth-of-type(2n) .document {
+  /* background-color: #ffffff; */
+}
+
+.section__subsection--highlight {
+  padding: 1em;
+  background-color: #f6ede2;
+  border-radius: 5px;
+  transform: rotate(-2deg);
+  margin: 3em 1em;
+}
+
+.vimeo-video__container {
+  width: calc(100%-5px);
+  height: 0;
+  padding-bottom: 56.25%;
+  position: relative;
+}
+
+.vimeo-video__iframe {
+  border: 1px solid #e5cdad;
+  border-radius: 5px;
+  position: absolute;
+  /* top: 0; */
+  /* left: 0; */
+  height: 100%;
+  width: 100%;
 }
 </style>
